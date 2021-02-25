b76cf021c5e62f82f0d6da60dfd20eadc7754e8d
Zouclar <tomas77680@gmail.com>
2021-02-25 15:56:10 +0100

    Ajout de fixtures et correction de l'affichage des produits en cas de non existance de l'id

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -2,16 +2,38 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
+use App\Entity\Shop;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Faker\Factory;
 
-class ProductFixtures extends Fixture
+class ProductFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
+        //TODO: récupérer tous les IDs des shops et en mettre un au hasard dans shop_id
+        $this->faker = Factory::create();
+        
+        for ($i = 0; $i < 100; $i++) {
+            $product = new Product();            
+            $product->setName($this->faker->word);
+            $product->setDescription($this->faker->sentence($nbWords = 15, $variableNbWords = true));
+            $product->setStock($this->faker->numberBetween($min = 2, $max = 599));
+            $manager->persist($product);
+
+
+
+        }
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            ShopFixtures::class,
+        ];
     }
 }
--- src/DataFixtures/ShopFixtures.php
+++ src/DataFixtures/ShopFixtures.php
@@ -2,16 +2,27 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Shop;
+use Faker\Factory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class ShopFixtures extends Fixture
 {
+    protected $faker;
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->faker = Factory::create();
 
+        for ($i = 0; $i < 100; $i++) {
+            $shop = new Shop();            
+            $shop->setName($this->faker->company);
+            $shop->setSiren($this->faker->randomNumber($nbDigits = 9, $strict = false));
+            $shop->setCapacity($this->faker->numberBetween($min = 2, $max = 9999));
+            $manager->persist($shop);
+        }
         $manager->flush();
+    
     }
 }
